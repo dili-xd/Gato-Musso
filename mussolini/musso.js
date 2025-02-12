4cf4f685149d53dd881544c9847969bd73ba4888
dili <edajmenesesanarca@gmail.com>
2025-02-12 15:54:27 -0600

    cambios

--- mussolini/musso.js
+++ mussolini/musso.js
@@ -8,13 +8,25 @@ const casilla6=document.getElementById("6")
 const casilla7=document.getElementById("7")
 const casilla8=document.getElementById("8")
 let casillas=[casilla0,casilla1,casilla2,casilla3,casilla4,casilla5,casilla6,casilla7,casilla8]
+let turnojugador=true
+
 function marcarcasilla(){
     casillas.forEach(casilla=>casilla.addEventListener("click", function(){
         if (casilla.innerHTML=="") {
            casilla.innerHTML="❤️" 
+           setTimeout(() => {
+               juegoAleatorio()
+           }, 500);
         }
     }))
+}
 
+function juegoAleatorio() {
+    let casillasVacias = casillas.filter(vacia=>  vacia.textContent == "")
+    let numeroAleatorio = Math.floor(Math.random() * casillasVacias.length);
+    casillasVacias[numeroAleatorio].textContent = "☀️"
 }
 
 marcarcasilla()
+
+
